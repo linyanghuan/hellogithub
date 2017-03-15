c2b4e4067a6c93d994694bd16b235942d416941b
linyanghuan <linyanghuan01@163.com>
2017-03-15 11:28:40 +0800

    b1 change

--- testmain.java
+++ testmain.java
@@ -5,7 +5,7 @@ public class testmain {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		System.out.println("ddddddsssttttttt");
+		System.out.println("ssssdddddsssttttttt");
 	}
 
 }
