3d60551abfcc70abc6b6525d77e18e0fdb868b7d
linyanghuan <linyanghuan01@163.com>
2017-03-15 11:20:30 +0800

    add1

--- testmain.java
+++ testmain.java
@@ -5,7 +5,7 @@ public class testmain {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		System.out.println("ddddddsss");
+		System.out.println("ddddddsssttttttt");
 	}
 
 }
